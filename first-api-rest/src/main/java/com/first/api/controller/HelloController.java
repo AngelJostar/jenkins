a06b6bb88ba43259866b3502c2ac4dc2c5a6f8bf
AngelJostar <angelrojas.ciencias@gmail.com>
2025-04-09 17:26:27 -0600

    Nuevos cambios

--- first-api-rest/src/main/java/com/first/api/controller/HelloController.java
+++ first-api-rest/src/main/java/com/first/api/controller/HelloController.java
@@ -41,7 +41,7 @@ public class HelloController {
         return "Hola Mundo con REST en Spring Boot";
     }
 
-        //Otro comentario
+        //Otro comentario 7
     @GetMapping("/hello7")
     public String saludar7(){
         return "Hola Mundo con REST en Spring Boot";
